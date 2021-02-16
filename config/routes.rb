76be712ff5d719afa3b267ce2f9342b93d7a3390
jamestucker1988 <xat2212@gmail.com>
2021-02-16 14:44:41 +0530

    work on submit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users , only: [:new , :create]
-  root 'users#new'
+  resource :user , only: [:new , :create]
+  root 'user#new'
  
 end
