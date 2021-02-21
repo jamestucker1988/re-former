992816cdf0a938a179072069ca33e383fc82d667
jamestucker1988 <xat2212@gmail.com>
2021-02-21 15:22:40 +0530

    edit controllers and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :user , only: [:new , :create]
-  root 'user#new'
- 
+  root 'users#new'
+  resources :users , only: [:new, :edit, :create, :update]
+  
 end
